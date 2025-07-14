60614d650e9f1202b2690a0a27ff7a88791542ff
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-07-14 12:07:09 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,1 +1,7 @@
 
+// Just an array of iframe URLs
+const videoLinks = [
+  "https://www.xerotica.com/embed/52356",
+  "https://www.xerotica.com/embed/52655",
+  "https://www.xerotica.com/embed/54154"
+];
