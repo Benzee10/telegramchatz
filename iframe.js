c96634de1c328aadf2fb3405a2aa515713c3d216
Benzee10 <168162718+Benzee10@users.noreply.github.com>
2025-09-09 12:23:06 +0100

    Delete iframe.js

--- iframe.js
+++
@@ -1,7 +0,0 @@
-
-// Just an array of iframe URLs
-const videoLinks = [
-  "https://www.xerotica.com/embed/52356",
-  "https://www.xerotica.com/embed/52655",
-  "https://www.xerotica.com/embed/54154"
-];
